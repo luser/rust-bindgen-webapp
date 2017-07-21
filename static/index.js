c3010f78c9cc52872747cdcd02218404d86dac58
Nick Fitzgerald <fitzgen@gmail.com>
2017-07-20 17:13:23 -0700

    Use semicolons dammit

--- static/index.js
+++ static/index.js
@@ -1,29 +1,33 @@
-const form = document.getElementById('source')
-const code = document.getElementById('bindings')
-form.onsubmit = (ev) => {
+const form = document.getElementById('source');
+const code = document.getElementById('bindings');
+
+form.addEventListener("submit", ev => {
   ev.preventDefault();
-  const body = new URLSearchParams()
-  const formData = new FormData(form)
+
+  const body = new URLSearchParams();
+  const formData = new FormData(form);
+
   // Rocket does not support multipart/form-data, this is a hack to use
   // application/x-www-form-urlencoded
-  for (const pair of formData.entries()) {
-    typeof pair[1]=='string' && body.append(pair[0], pair[1])
+  for (const [k, v] of formData.entries()) {
+    if (typeof k == 'string') {
+      body.append(k, v);
+    }
   }
-  const headers = new Headers()
-  const requestInit = {
+
+  const request = new Request('/api/bindgen', {
     method: 'POST',
-    headers: headers,
     body: body
-  }
-  const request = new Request('/api/bindgen', requestInit)
+  });
+
   fetch(request).then((response) => {
     if (!response.ok) {
-      throw Error("Failed to generate bindings.")
+      throw Error("Failed to generate bindings.");
     }
     return response.text();
   }).then((bindings) => {
-    code.innerText = bindings
+    code.innerText = bindings;
   }).catch((error) => {
-    code.innerText = error
+    code.innerText = error;
   })
-}
+});
