292bb4fab511d5d85a9e1dc86a2447bc30c12396
Nick Fitzgerald <fitzgen@gmail.com>
2017-07-20 17:09:05 -0700

    Use `const` instead of `var`
    Its 2017

--- static/index.js
+++ static/index.js
@@ -1,21 +1,21 @@
-var form = document.getElementById('source')
-var code = document.getElementById('bindings')
+const form = document.getElementById('source')
+const code = document.getElementById('bindings')
 form.onsubmit = (ev) => {
   ev.preventDefault();
-  var body = new URLSearchParams()
-  var formData = new FormData(form)
+  const body = new URLSearchParams()
+  const formData = new FormData(form)
   // Rocket does not support multipart/form-data, this is a hack to use
   // application/x-www-form-urlencoded
-  for (var pair of formData.entries()) {
+  for (const pair of formData.entries()) {
     typeof pair[1]=='string' && body.append(pair[0], pair[1])
   }
-  var headers = new Headers()
-  var requestInit = {
+  const headers = new Headers()
+  const requestInit = {
     method: 'POST',
     headers: headers,
     body: body
   }
-  var request = new Request('/api/bindgen', requestInit)
+  const request = new Request('/api/bindgen', requestInit)
   fetch(request).then((response) => {
     if (!response.ok) {
       throw Error("Failed to generate bindings.")
